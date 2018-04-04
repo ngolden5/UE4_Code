c7a2147f912c8469063200de9271f719ed70cf5c
Nick-Desktop\Nick <ngolden5@student.gsu.edu>
2018-04-04 01:40:29 -0400

    4/4 1:20 AM, Finally got it to work.

--- Section_03/BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ Section_03/BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,3 +1,44 @@
+<<<<<<< HEAD
+// Fill out your copyright notice in the Description page of Project Settings.
+//#include "BuildingEscapeGameMode.h" if 4.17+, has to be first.
+#include "BuildingEscape.h"
+#include "PositionReporter.h"
+//#include "Gameframework/Actor.h" Use this if 4.16 and above.
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+	
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay(); //Do whatever is up the inheritance tree. Do this first. Generated.
+	//Runs once at the start of the game.
+	FString ObjectName = GetOwner()->GetName(); //* is overloaded.
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s"), *ObjectName); //UE_LOG and TEXT are macros.
+	//Macros are scary.
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+=======
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
@@ -33,3 +74,4 @@ void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 	// ...
 }
 
+>>>>>>> origin/master
--- Section_03/BuildingEscape/Source/BuildingEscape/PositionReporter.h
+++ Section_03/BuildingEscape/Source/BuildingEscape/PositionReporter.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
@@ -26,3 +27,33 @@ public:
 		
 	
 };
+=======
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "PositionReporter.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UPositionReporter : public UActorComponent //inherits from
+{
+	GENERATED_BODY() //Unreal header tool to do magic
+
+public:	
+	// Sets default values for this component's properties
+	UPositionReporter();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override; //We're not saying how it works here. Probably synomous with abstract in Java.
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
+>>>>>>> origin/master
