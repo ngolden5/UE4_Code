effce89491162e3fbfb1bd7d701c3fcaa7e3ad72
DESKTOP-KFQBD0N\Goldennick94 <goldennick94@gmail.com>
2018-04-09 18:12:29 -0400

    4/9 Update
    OpenDoor class created.
    Changed NewMap.

---
+++ Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Nicholas Golden (c) 2018
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find the owning Actor
+	AActor* Owner = GetOwner();
+	//Create a rotator
+	FRotator NewRotation = FRotator(0.0f, 60.0f, 0.0f);
+
+	//Set the door rotation
+	Owner->SetActorRotation(NewRotation);
+	//Debug code
+	FString ObjectName = Owner->GetName(); //* is overloaded.
+	FString ObjectRotate = Owner->GetTransform().GetRotation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectRotate);
+
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
